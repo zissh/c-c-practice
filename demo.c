876fb65d7f6d2f2b910a9e4641a77413109b7885
zissh <15775926569@163.com>
2020-05-25 20:13:23 +0800

    Q&A框架模板

---
+++ demo.c
@@ -0,0 +1,15 @@
+//+++++++++++++++++++question+++++++++++++++++++
+//
+//输出Hello World
+//
+//+++++++++++++++++++solution+++++++++++++++++++
+
+#include <stdio.h>
+int main()
+{
+   /* 我的第一个 C 程序 */
+   printf("Hello, World! \n");
+   return 0;
+}
+//+++++++++++++++++++results++++++++++++++++++++
+//Hello, World! 
